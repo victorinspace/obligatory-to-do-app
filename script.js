40e42d8978af52ab3254e972d702f4ac235a15c5
Victor <victorv24@gmail.com>
2021-03-09 20:26:10 -0800

    Newly added tasks are displaying on the page underneath any previously created task items.

--- script.js
+++ script.js
@@ -4,22 +4,43 @@
 let addTaskItemButton = document
     .getElementById("add-task-button")
     .addEventListener("click", storeTaskItem);
+let incrementerObject = 0;
+
+let taskItemContainer = {};
 
 function storeTaskItem(e) {
+    incrementerObject++;
     
     let taskItem = document.getElementById('task-item').value;
     
+    if (typeof(Storage) !== 'undefined') {
+        localStorage.setItem(incrementerObject, taskItem);
+        console.log('local storage:' + localStorage.getItem(incrementerObject) + incrementerObject);
+    }
+
     if (taskItem == '') {
         alert('I need orders!');
         return false;
     }
     
-    let output = document.getElementById("list-tasks");
-    output.innerHTML = `<li>${taskItem}</li>`;
+    let showListContainer = document.getElementById("list-tasks");
+    let li = document.createElement('li');
+    li.appendChild(
+        document.createTextNode(localStorage.getItem(incrementerObject))
+    );
+    showListContainer.appendChild(li);
+
+    // let displayNewItem = showListContainer.innerHTML = `
+    //     <li>${localStorage.getItem(incrementerObject)}</li>
+    // `;
+
+
 }
 
 
 
+// to delete the localstorage String
+// localStorage.removeItem("name");
 
 
 
