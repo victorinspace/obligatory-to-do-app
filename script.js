b46036468d5f6fc7f01efff283bfa585b3e71063
Victor <victorv24@gmail.com>
2021-03-10 18:58:25 -0800

    Light reformatting and logic organizing.

--- script.js
+++ script.js
@@ -1,50 +1,49 @@
 // TO-DO: 
-// - Figure out how they can press 'Enter' to add item
+// - ability to delete added items
+// - create delete button on new task item elements
+// - create 'done' button on new task item elements
+
+const testOutput = function (name, out) {
+    console.log(`${name}: ${out}`);
+};
 
 let addTaskItemButton = document
     .getElementById("add-task-button")
     .addEventListener("click", storeTaskItem);
-let incrementerObject = 0;
 
-let taskItemContainer = {};
+let incrementerObject = 0;
 
 function storeTaskItem(e) {
     incrementerObject++;
     
-    let taskItem = document.getElementById('task-item').value;
+    // grab the new item
+    let taskItem = document.getElementById('new-task-item').value;
     
-    if (typeof(Storage) !== 'undefined') {
-        localStorage.setItem(incrementerObject, taskItem);
-        console.log('local storage:' + localStorage.getItem(incrementerObject) + incrementerObject);
-    }
-
-    if (taskItem == '') {
+    // check if input is blank
+    if (taskItem == "") {
         alert('I need orders!');
         return false;
     }
+
+    // store the item
+    if (typeof(Storage) !== 'undefined') {
+        localStorage.setItem(incrementerObject, taskItem);
+    }
     
-    let showListContainer = document.getElementById("list-tasks");
+    // print out the task item
+    let taskItemListView = document.getElementById("list-tasks");
+
     let li = document.createElement('li');
+
     li.appendChild(
         document.createTextNode(localStorage.getItem(incrementerObject))
     );
-    showListContainer.appendChild(li);
 
-    // let displayNewItem = showListContainer.innerHTML = `
-    //     <li>${localStorage.getItem(incrementerObject)}</li>
-    // `;
+    testOutput('event', li);
 
-
+    taskItemListView.appendChild(li);
 }
-
-
 
 // to delete the localstorage String
 // localStorage.removeItem("name");
 
-
-
-
-// display the persons added tasks
-// let taskItemDisplay = document.createElement('li');
-// document.getElementById('list-tasks').append(taskItem)
