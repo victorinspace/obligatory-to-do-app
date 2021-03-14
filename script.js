592d8efdc261038b68fc5d046855e1344a32c5fb
Victor <victorv24@gmail.com>
2021-03-14 15:56:31 -0700

    Each entry is creating it's own key/value pair in the localStorage.

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 // TO-DO: 
-// - successfully listen to delete button click
-// - successfully delete task item from list
-
+// 1. A user should be able to mark a TO-DO as completed
+// 2. When a user mark’s a TO-DO as completed, the text of 
+//      the TO-DO should be crossed out (using strikethrough font)
+// 3. A user should have the option of deleting TO-DO’s 
+//      that have been mark completed
 
 let addTaskItemButton = document
     .getElementById("add-task-button")
     .addEventListener("click", saveAndPrintTaskItem);
 
+let createKey = 0;
+
 function saveAndPrintTaskItem() {
+    createKey++;
     // ** grab and save the new task item from the input field
     let taskItem = document.getElementById("new-task-item").value;
     // -- check to see if the input is blank
@@ -15,39 +20,15 @@ function saveAndPrintTaskItem() {
         alert("I need orders!");
         return false;
     }
-    localStorage.setItem("taskItem", taskItem);
-    let grabTaskItem = localStorage.getItem("taskItem");
+    localStorage.setItem(createKey, taskItem);
+    let grabTaskItem = localStorage.getItem(createKey);
     console.log("added task item: ", grabTaskItem);
 
     // ** print the task item to the page
     let taskContainer = document.getElementById("list-tasks");
     let li = document.createElement("li");
     li.appendChild(document.createTextNode(grabTaskItem));
-    createDeleteButton();
     taskContainer.appendChild(li);
-    taskContainer.appendChild(deleteItem);
 }
 
-// ** DELETE BUTTON **
-let deleteItem = document.createElement("button");
-const createDeleteButton = function() {
-    // append the "X" for the delete button
-    deleteItem.appendChild(document.createTextNode("X"));
-    deleteItem.classList.add("remove-item");
-}
-
-
-
-
-
-
-// // listen for delete click
-// let letThemDelete = document.getElementsByClassName("remove-item");
-
-// letThemDelete[0].addEventListener("click", removeTaskItem);
-
-
-
-// to delete the localstorage String
-// localStorage.removeItem("name");
 
