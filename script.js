af8179ba960c82dd0f6f392f207733ea6db53958
Victor <victorv24@gmail.com>
2021-03-15 20:24:08 -0700

    User can delete items!

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// TO-DO: 
+// ** TO-DO: 
 // 1. A user should be able to mark a TO-DO as completed
 // 2. When a user mark’s a TO-DO as completed, the text of 
 //      the TO-DO should be crossed out (using strikethrough font)
@@ -9,29 +9,21 @@ let addTaskItemButton = document
     .getElementById("add-task-button")
     .addEventListener("click", saveAndPrintTaskItem);
 
-let deleteTaskItem = function() {
-    // localStorage.removeItem(item);
-    let listenForDelete = document.getElementById(createKey);
-    listenForDelete.addEventListener("click", testDeleteFunction);
-
-    function testDeleteFunction() {
-        console.log("delete clicked! ", listenForDelete);
-    }
-}
-
 let createKey = 0;
 
-function saveAndPrintTaskItem() {
+function saveAndPrintTaskItem(e) {
+    e.preventDefault();
     createKey++;
+    
     // ** grab and save the new task item from the input field
     let taskItem = document.getElementById("new-task-item").value;
-    // -- check to see if the input is blank
+        // check to see if the input is blank
     if (taskItem == "") {
         alert("I need orders!");
         return false;
     }
-    localStorage.setItem(createKey, taskItem);
-    let grabTaskItem = localStorage.getItem(createKey);
+    sessionStorage.setItem(createKey, taskItem);
+    let grabTaskItem = sessionStorage.getItem(createKey);
     console.log(`added task item! key: ${createKey}, value: ${grabTaskItem}`);
 
     // ** print the task item to the page and the delete button
@@ -41,10 +33,14 @@ function saveAndPrintTaskItem() {
     taskContainer.appendChild(li);
     let deleteButton = document.createElement("button");
     deleteButton.appendChild(document.createTextNode("X"));
-    deleteButton.setAttribute("id", createKey)
+    deleteButton.setAttribute("id", createKey);
+    deleteButton.setAttribute("onclick", "didUserDelete(this.id)")
     deleteButton.classList.add('delete-button');
     li.appendChild(deleteButton);    
-
-    
 }
 
+function didUserDelete(e) {
+    let deletedListElement = document.getElementById(e).parentElement;
+    deletedListElement.remove();
+    sessionStorage.removeItem(e);
+}
